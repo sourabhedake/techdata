a58d5e052ae163cb2e22ef44d1080d7f8abf7764
Sourabh Edake <sdnedake@gmail.com>
2019-08-06 21:45:31 +0530

    Code Refactored

--- webui/src/app/pages/quiz/quiz.component.ts
+++ webui/src/app/pages/quiz/quiz.component.ts
@@ -2,6 +2,9 @@ import {
   Component,
   OnDestroy
 } from '@angular/core';
+import {
+  RestClientService
+} from '../../@core/utils';
 
 @Component({
   selector: 'ngx-quiz',
@@ -16,7 +19,7 @@ export class QuizComponent implements OnDestroy {
     ARCHIVE: 'archive',
   }
 
-  quizList = {
+  public quizList = {
     archive: {
       quiz: [],
       quizBucket: [],
@@ -38,69 +41,72 @@ export class QuizComponent implements OnDestroy {
     pageToLoadNext: 1,
   };
 
-  pushIntoQuiz(q: any, newQuiz: any) {
-    if (q.quiz.length === 0) {
-      q.quiz = [newQuiz];
-    } else {
-      q.quiz.push(newQuiz);
-    }
+  private pushIntoQuiz(q: any, newQuiz: any) {
+    q.quiz.push(newQuiz);
     q.count++;
 
-    this.pushIntoQuizBucket(q, newQuiz, 6);
+    this.pushIntoQuizArchiveBucket(q, newQuiz, 6);
   }
 
-  pushIntoQuizBucket(q: any, newQuiz: any, bucketSize: Number) {
-    console.log(q.quizBucket)
-    console.log(q.quizBucket.length)
-    if (q.quizBucket.length === 0) {
-      q.quizBucket = [
-        [
-          newQuiz
-        ]
-      ];
+  private pushIntoQuizArchiveBucket(q: any, newQuiz: any, bucketSize: Number) {
+    if (!q.quizBucket[0]) {
+      q.quizBucket[0] = [newQuiz];
+
     } else if (q.quizBucket[q.quizBucket.length - 1].length >= bucketSize) {
       q.quizBucket.push([newQuiz]);
+
     } else {
       q.quizBucket[q.quizBucket.length - 1].push(newQuiz)
     }
   }
 
-  constructor() {}
+  constructor(private rc: RestClientService) {
+    this.loadNextActive();
+    this.loadNextArchive();
+    this.loadNextUpcoming();
+  }
 
-  loadNextArchive() {
+  public loadNextArchive() {
     if (this.quizList.archive.loading) {
       return;
     }
     this.quizList.archive.loading = true;
-    this.pushIntoQuiz(this.quizList.archive, {
-      'title': 'title',
-      'text': this.quizList.archive.count
-    });
+    this.rc.call(this.rc.p().QUIZ_GET, ['ARCHIVE'])
+    .pipe()
+      .subscribe(data => {
+        console.log("donee" + data);
+        this.pushIntoQuiz(this.quizList.archive, {
+          'title': 'title',
+          'text': 'archive' + this.quizList.archive.count
+        });
+      }, (err) => {
+          console.log("Error: ", err.error);
+      });
     this.quizList.archive.loading = false;
   }
 
   loadNextUpcoming() {
-    if (this.quizList.upcoming.loading) {
-      return;
-    }
-    this.quizList.upcoming.loading = true;
-    this.pushIntoQuiz(this.quizList.upcoming, {
-      'title': 'title',
-      'text': this.quizList.upcoming.count
-    });
-    this.quizList.upcoming.loading = false;
+    // if (this.quizList.upcoming.loading) {
+    //   return;
+    // }
+    // this.quizList.upcoming.loading = true;
+    // this.pushIntoQuiz(this.quizList.upcoming, {
+    //   'title': 'title',
+    //   'text': this.quizList.upcoming.count
+    // });
+    // this.quizList.upcoming.loading = false;
   }
 
   loadNextActive() {
-    if (this.quizList.active.loading) {
-      return;
-    }
-    this.quizList.active.loading = true;
-    this.pushIntoQuiz(this.quizList.active, {
-      'title': 'title',
-      'text': this.quizList.active.count
-    });
-    this.quizList.active.loading = false;
+    // if (this.quizList.active.loading) {
+    //   return;
+    // }
+    // this.quizList.active.loading = true;
+    // this.pushIntoQuiz(this.quizList.active, {
+    //   'title': 'title',
+    //   'text': this.quizList.active.count
+    // });
+    // this.quizList.active.loading = false;
   }
 
   ngOnDestroy() {}
--- webui/src/app/pages/quiz/quiz.module.ts
+++ webui/src/app/pages/quiz/quiz.module.ts
@@ -10,6 +10,7 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { QuizComponent } from './quiz.component';
 import { QuizCardComponent } from './quiz-card/quiz.component';
 import { QuizBoardComponent } from './quiz-board/quiz.component';
+import { RestClientService } from '../../@core/utils';
 
 @NgModule({
   imports: [
@@ -28,5 +29,8 @@ import { QuizBoardComponent } from './quiz-board/quiz.component';
     QuizCardComponent,
     QuizBoardComponent,
   ],
+  providers: [
+    RestClientService
+  ]
 })
 export class QuizModule { }
